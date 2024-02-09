feb6be0a8a58592deaae946ef3047602fdd90a41
Ivan Cardozo <ivancardozo11@hotmail.com>
2024-02-09 01:32:37 +0100

    Implementent code refactor and optimitations to rate-limit, now anyone can se avaliable monsters but limited to a couple requests

--- src/common/interceptors/rate-limit.interceptor.ts
+++ src/common/interceptors/rate-limit.interceptor.ts
@@ -15,29 +15,15 @@ export class RateLimitInterceptor implements NestInterceptor {
         const request = ctx.getContext().req || ctx.getContext();
         const apiKey: string | undefined = request.headers['x-api-key'];
 
-        if (!apiKey) {
-            throw new HttpException('API key is required, you reached your petition limit', HttpStatus.BAD_REQUEST);
-        }
-
-        const validApiKeys = [
-            this.configService.get<string>('BORED_MIKE_API_KEY'),
-            this.configService.get<string>('CEO_API_KEY'),
-            this.configService.get<string>('EMPLOYEE_API_KEY')
-        ];
-
-        if (!validApiKeys.includes(apiKey)) {
-            throw new HttpException('Invalid API key', HttpStatus.UNAUTHORIZED);
-        }
-
         const key: string = `rateLimit:${apiKey}`;
         const current = await this.cacheService.get(key);
         const requests: number = current ? parseInt(current, 10) : 0;
 
         if (requests >= 10) {
-            throw new HttpException('Rate limit exceeded', HttpStatus.TOO_MANY_REQUESTS);
+            throw new HttpException('Rate limit exceeded - Try again in a minute', HttpStatus.TOO_MANY_REQUESTS);
         }
 
-        await this.cacheService.set(key, (requests + 1).toString(), 3600);
+        await this.cacheService.set(key, (requests + 1).toString(), 60);
 
         return next.handle().pipe(
             tap(() => console.log(`Request processed for field: ${ctx.getInfo().fieldName}`))
