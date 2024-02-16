4813bc639f7aaab15984d59b47b78eec4fbe8772
Ivan Cardozo <ivancardozo11@hotmail.com>
2024-02-16 04:53:47 +0100

    modify redis cloud connection string

--- src/common/services/cache.service.ts
+++ src/common/services/cache.service.ts
@@ -8,8 +8,9 @@ export class CacheService {
 
     constructor () {
         this.client = new Redis({
-            host: process.env.REDIS_HOST || 'localhost',
-            port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6379
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6379,
+            password: process.env.REDIS_PASSWORD
         });
     }
 
